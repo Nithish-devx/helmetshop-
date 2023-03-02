ffdf9b348d1c433ca58d597d920ba121b3d2d2ad
Nithish-devx <125951185+Nithish-devx@users.noreply.github.com>
2023-03-02 09:27:12 +0530

    Add files via upload

---
+++ leecc1/src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.entity;
+import com.example.demo.service.service;
+
+
+@RestController
+public class controller {
+
+	@Autowired
+	service s;
+
+	@GetMapping("/get")
+	public List<entity> showDetails() {
+		return s.getDetails();
+	}
+
+	@PostMapping("/post")
+	public String addDetails(@RequestBody entity m) {
+		s.add(m);
+		return "Added product " + m.getId();
+	}
+	@PutMapping("/update")
+	public entity updateInfo(@RequestBody entity st) {
+		return s.updateDetails(st);
+	}
+	@DeleteMapping("/delete")
+	public void deleteInfo(@RequestBody entity st2) {
+		s.deleteDetails(st2);
+	}
+	
+
+	//sorting
+	@GetMapping("/product/{field}")
+	public List<entity> getWithSort(@PathVariable String field) {
+		return s.getSorted(field);
+	}
+
+	// pagination
+	@GetMapping("/product/{offset}/{pageSize}")
+	public List<entity> productsWithPagination(@PathVariable int offset, @PathVariable int pageSize) {
+		return s.getWithPagination(offset, pageSize);
+	}
+}
